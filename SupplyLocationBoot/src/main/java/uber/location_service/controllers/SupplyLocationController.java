0da70e04c6cdb9dfb6c0470b5490c18ab47942f7
Hlib Pylypets <pilipets.gleb@gmail.com>
2020-10-31 00:47:44 +0200

    Added SupplyLocationImpl service, exception handling.
    Signed-off-by: Hlib Pylypets <pilipets.gleb@gmail.com>

--- SupplyLocationBoot/src/main/java/uber/location_service/controllers/SupplyLocationController.java
+++ SupplyLocationBoot/src/main/java/uber/location_service/controllers/SupplyLocationController.java
@@ -2,60 +2,59 @@ package uber.location_service.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.javatuples.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uber.location_service.services.SupplyLocationImpl;
+import uber.location_service.structures.GeoPoint;
 import uber.location_service.structures.SupplyInstance;
 import uber.location_service.structures.Temp;
-import uber.location_service.utils.JsonParseHandler;
-import uber.location_service.structures.GeoPoint;
-import uber.location_service.utils.JsonParseHandler;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController()
 @RequestMapping(path="/supply_location")
 public class SupplyLocationController {
+   SupplyLocationImpl impl;
+
+   public SupplyLocationController(final SupplyLocationImpl impl) {
+      this.impl = impl;
+   }
+
    @GetMapping(path="/get_closest")
    public ResponseEntity<Object> getClosestHandler(
-         @RequestParam(value = "geoPoint") String geoPointString) {
-      final GeoPoint geoPoint = JsonParseHandler.parseJson(geoPointString, GeoPoint.class);
-      if (geoPoint == null) {
-         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-      }
-      return new ResponseEntity<>(geoPoint, HttpStatus.OK);
+         @RequestParam(value = "geoPoint") String geoPointString) throws JsonProcessingException {
+      final GeoPoint geoPoint = new ObjectMapper().readValue(geoPointString, GeoPoint.class);
+
+      List<Pair<SupplyInstance, Double>> arr = impl.getClosestSupply(geoPoint);
+      return new ResponseEntity<>(arr, HttpStatus.OK);
    }
 
    @GetMapping(path="/get_closest_in_radius")
    public ResponseEntity<Object> getClosestInRadiusHandler(
-         @RequestParam(value = "geoPoint") String geoPointString) {
-      final GeoPoint geoPoint = JsonParseHandler.parseJson(geoPointString, GeoPoint.class);
-      if (geoPoint == null) {
-         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-      }
-      List<GeoPoint> arr = new ArrayList<>();
-      arr.add(geoPoint); arr.add(geoPoint); arr.add(geoPoint);
+         @RequestParam(value = "geoPoint") String geoPointString) throws JsonProcessingException {
+      final GeoPoint geoPoint = new ObjectMapper().readValue(geoPointString, GeoPoint.class);
+
+      List<Pair<SupplyInstance, Double>> arr = impl.getRadiusSupply(geoPoint);
       return new ResponseEntity<>(arr, HttpStatus.OK);
    }
 
+   /*
    @GetMapping(path="/get_temp")
    public ResponseEntity<Object> greeting2(
-         @RequestParam(value = "temp") String tempString) {
-      final Temp temp = JsonParseHandler.parseJson(tempString, Temp.class);
-      if (temp == null) {
-         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-      }
+         @RequestParam(value = "temp") String tempString) throws JsonProcessingException {
+      final Temp temp = new ObjectMapper().readValue(tempString, Temp.class);
+
       return new ResponseEntity<>(temp, HttpStatus.OK);
-   }
+   }*/
 
    @PostMapping(path="/update_supply")
    public ResponseEntity<Object> updateSupplyLocation(
-         @RequestParam(value = "instance") String supplyString) {
-      final SupplyInstance ins = JsonParseHandler.parseJson(supplyString, SupplyInstance.class);
-      if (ins == null) {
-         return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-      }
-      return new ResponseEntity<>(ins, HttpStatus.OK);
+         @RequestParam(value = "instance") String supplyString) throws JsonProcessingException {
+      final SupplyInstance ins = new ObjectMapper().readValue(supplyString, SupplyInstance.class);
+
+      impl.update(ins);
+      return new ResponseEntity<>(HttpStatus.OK);
    }
 }
---
+++ SupplyLocationBoot/src/main/java/uber/location_service/services/SupplyLocationImpl.java
@@ -0,0 +1,66 @@
+package uber.location_service.services;
+
+import org.javatuples.Pair;
+import org.springframework.stereotype.Service;
+import uber.location_service.algo.callable.ClosestSupplyCallable;
+import uber.location_service.algo.callable.RadiusSupplyCallable;
+import uber.location_service.structures.GeoPoint;
+import uber.location_service.structures.SupplyInstance;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.*;
+
+@Service
+public class SupplyLocationImpl {
+   final int corePoolSize = 3;
+   final int maximumPoolSize = 10;
+   final int keepAliveTime = 120; // sec
+   final int waitQueueCapacity = 1000;
+
+   // In the future might be replaced with S2 library and KD-Tree
+   // internal cache container
+   protected ConcurrentHashMap<UUID, SupplyInstance> lhm = new ConcurrentHashMap<>(100);
+   protected ThreadPoolExecutor executorService;
+
+   public SupplyLocationImpl() {
+      executorService = new ThreadPoolExecutor(
+            corePoolSize,
+            maximumPoolSize,
+            keepAliveTime,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(waitQueueCapacity)
+      );
+   }
+
+   public void update(SupplyInstance val) {
+      lhm.put(val.getUUID(), val);
+   }
+
+
+   public List<Pair<SupplyInstance, Double>> getRadiusSupply(GeoPoint location) {
+      try {
+         Future<List<Pair<SupplyInstance, Double>>> future =
+               executorService.submit(new RadiusSupplyCallable(lhm, location));
+         return future.get();
+      } catch (RejectedExecutionException | InterruptedException | ExecutionException ex) {
+         ex.printStackTrace();
+         return null;
+      }
+   }
+
+   public List<Pair<SupplyInstance, Double>> getClosestSupply(GeoPoint location) {
+      try {
+         Future<List<Pair<SupplyInstance, Double>>> future =
+               executorService.submit(new ClosestSupplyCallable(lhm, location));
+         return future.get();
+      } catch (RejectedExecutionException | InterruptedException | ExecutionException ex) {
+         ex.printStackTrace();
+         return null;
+      }
+   }
+
+   public GeoPoint getSupplyLocation(UUID uuid) {
+      return lhm.get(uuid).getLocation();
+   }
+}
