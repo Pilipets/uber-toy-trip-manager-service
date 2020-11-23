21a3bd4cb7425443e524c6fb5484c0320b74c68b
Hlib Pylypets <pilipets.gleb@gmail.com>
2020-11-23 03:15:33 +0200

    Moved supply location to separate repo

--- SupplyLocationService/src/main/java/uber/location_service/SupplyLocationService.java
+++
@@ -1,9 +0,0 @@
-package uber.location_service;
-
-class SupplyLocationService {
-   public static void main(String[] args) {
-      System.out.println("Hello world");
-   }
-
-   SupplyLocationImpl impl = new SupplyLocationImpl();
-}
